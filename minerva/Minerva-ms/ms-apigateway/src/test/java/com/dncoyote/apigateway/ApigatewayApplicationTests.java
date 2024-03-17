a85828cd36646c13e2517f52cf31e7ed07cb06c7
BilalThaha <bilal.athaha@gmail.com>
2024-03-17 22:26:49 +0530

    apigateway

---
+++ minerva/Minerva-ms/ms-apigateway/src/test/java/com/dncoyote/apigateway/ApigatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dncoyote.apigateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApigatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
